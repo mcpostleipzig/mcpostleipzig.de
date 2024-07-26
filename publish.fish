f19e8be80cda822e2e2d7cf4042ee18e04aeb76c
Aramís Concepción Durán <aramis@systemli.org>
2024-07-26 20:14:22 +0200

    Updated from Publii

--- publish.fish
+++ publish.fish
@@ -1,10 +1,18 @@
 #!/usr/bin/env fish
 
-# mkdir -p alt
-# mv * alt 2>/dev/null
-# mv alt/mc-post-leipzig-ev-im-adac-files/* .
-# mv alt/publish.fish .
-# rm -rf alt
+if not test -d mc-post-leipzig-ev-im-adac-files
+    echo Ich benötige das Verzeichnis ./mc-post-leipzig-ev-im-adac-files/
+    echo Beende!
+    exit
+end
+
+mkdir -p alt
+mv * alt 2>/dev/null
+mv alt/mc-post-leipzig-ev-im-adac-files/* .
+mv alt/publish.fish .
+rm -rf alt
+
+echo www.mcpostleipzig.de >CNAME
 
 git add *
 git commit -m "Updated from Publii"
